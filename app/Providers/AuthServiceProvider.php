2a7ffaf2462968a5bc8eb0af405c53f5f3eb9357
Everton <everton.h.g.fonseca@gmail.com>
2022-09-26 18:13:08 -0300

    loop de registrar policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,8 +5,11 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 
 use App\Models\Aluno;
+use App\Models\Turma;
 use App\Policies\AlunoPolicy;
+use App\Policies\TurmaPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -16,18 +19,29 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        Aluno::class => AlunoPolicy::class,
+        // Aluno::class => AlunoPolicy::class,
+        // Turma::class => TurmaPolicy::class,
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
+    // /**
+    //  * Register any authentication / authorization services.
+    //  *
+    //  * @return void
+    //  */
+    // public function boot()
+    // {
+    //     $this->registerPolicies();
 
-        //
+    //     //
+    // }
+
+    public function boot()
+
+    {
+        // Automatically finding the Policies
+        Gate::guessPolicyNamesUsing(function ($modelClass) {
+            return 'App\\Policies\\' . class_basename($modelClass) . 'Policy';
+        });
+        $this->registerPolicies();
     }
 }
